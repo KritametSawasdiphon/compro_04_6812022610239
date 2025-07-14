0ea9fbf73fee16853ff50e25c14b5bdaed24281d
KritametSawasdiphon <s6812022610239@email.kmutnb.ac.th>
2025-07-14 17:01:54 +0700

    งานส่งใหม่ล่าสุด

---
+++ coding_w04-03.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+int main() {
+    char nameInitial = 'K';       
+    int age = 20;                 
+    float weight = 48.5;          
+    char gender[] = "Female";     
+    printf("= %c nameInitial\n", nameInitial); 
+    printf("age = %d\n", age);                  
+    printf("weight = %.2f\n", weight);          
+    printf("gender = %s\n", gender);           
+    return 0;  
+}
